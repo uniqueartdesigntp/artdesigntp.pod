3efe11cd56213b46011a54899f336f46b118ccbc
uniqueartdesigntp <unique.artdesigntp@yahoo.com>
2025-07-15 17:17:01 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>ArtDesignTP POD Landing Page</h1>
+      <p>Professional Print On Demand WebApp fully compatible with Amazon Developer.</p>
+    </div>
+  );
+};
+
+export default App;
